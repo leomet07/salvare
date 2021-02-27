d7d218abf83de8d5d69cef46a08f61af72a3bbf0
leomet07 <leomet07@gmail.com>
2021-02-27 12:43:45 -0500

    Basic occurence functionality

--- server/routes/api/db/db.js
+++ server/routes/api/db/db.js
@@ -1,20 +1,29 @@
 const router = require("express").Router();
 const Occurrence = require("../../../model/Occurrence");
 router.get("/get_occurrences", async (req, res) => {
-	const occurrences = await Occurrence.find(req.body);
+	const occurrences = await Occurrence.find(req.body || {});
 	res.json(occurrences);
 });
 router.post("/create_occurrences", async (req, res) => {
-	// const schema = await Occurrence.create(req.body);
 	const neo_reference_id = req.body.neo_reference_id;
 	// Needs to be unique for every neo_reference_id
-	const schema = await Occurrence.findOneAndUpdate(
-		{ neo_reference_id: neo_reference_id },
-		req.body,
-		{
-			upsert: true,
-		}
-	);
+	const isExisting = await Occurrence.find({
+		neo_reference_id: neo_reference_id,
+	});
+	let schema;
+
+	if (isExisting.length > 0) {
+		schema = await Occurrence.findOneAndUpdate(
+			{ neo_reference_id: neo_reference_id },
+			req.body,
+			{
+				upsert: true,
+			}
+		);
+	} else {
+		schema = await Occurrence.create(req.body);
+	}
+
 	const occurrence_saved = await schema.save();
 	res.json(occurrence_saved);
 });
