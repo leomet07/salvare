999e1d3ae7ba59a5d9183eec0f07ea21f5d8ab72
leomet07 <leomet07@gmail.com>
2021-02-27 21:18:18 -0500

    Spacing fix

--- web/src/pages/About/About.js
+++ web/src/pages/About/About.js
@@ -37,14 +37,14 @@ export default function About() {
 				</h4>
 				<h4></h4>
 				<h4>
-					Credit to NASA for NeoWs API:
+					Credit to NASA for NeoWs API:&nbsp;
 					<a href="https://api.nasa.gov" target="_blank">
 						api.nasa.gov
 					</a>
 				</h4>
 
 				<h4>
-					Generated graphs from
+					Generated graphs from:&nbsp;
 					<a href="https://ssd.jpl.nasa.gov/sbdb.cgi" target="_blank">
 						JPL
 					</a>
